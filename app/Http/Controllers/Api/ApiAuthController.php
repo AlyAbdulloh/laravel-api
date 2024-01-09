4e4a5c0692677aaf25fd2a6be5dc20f27dd52334
AlyAbdulloh <uwongsabar12@gmail.com>
2024-01-09 11:02:36 +0700

    feat: add function logout in AuthController

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Resources\LoginResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ApiAuthController extends Controller
@@ -14,23 +15,47 @@ class ApiAuthController extends Controller
     //
     function login(LoginRequest $request)
     {
-        // check if user exists
-        $user = User::where('email', $request->email)->first();
+        // // check if user exists
+        // $user = User::where('email', $request->email)->first();
 
-        //check if password is correct
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        // //check if password is correct
+        // if (!$user || !Hash::check($request->password, $user->password)) {
 
+        //     return response()->json([
+        //         'massage' => 'Bad credentials'
+        //     ], 401);
+        // }
+
+        // //generate token
+        // $token = $user->createToken('token')->plainTextToken;
+
+        // //return response json
+        // return  new LoginResource([
+        //     'token' => $token,
+        // ]);
+
+        //validate dengan Auth::attempt
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            // jika berhasil buat token
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('token')->plainTextToken;
+            return  new LoginResource([
+                'token' => $token,
+            ]);
+        } else {
             return response()->json([
                 'massage' => 'Bad credentials'
             ], 401);
         }
+    }
 
-        //generate token
-        $token = $user->createToken('token')->plainTextToken;
+    function logout(Request $request)
+    {
+        // $request->user()->currentAccessToken()->delete();
 
-        //return response json
-        return  new LoginResource([
-            'token' => $token,
-        ]);
+        $request->user()->tokens()->delete();
+
+        return response()->noContent();
     }
 }
